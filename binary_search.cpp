4facbe0a75d0d9c4798e34ee74e909b42b71000a
Rizhuycihuy <rizki003131@gmail.com>
2026-04-16 18:09:39 +0700

    add void display

--- binary_search.cpp
+++ binary_search.cpp
@@ -48,3 +48,20 @@ void bubbleSortArray()
     } while (pass <= nPanjang - 1);
 }   
 
+void display()
+{
+    cout << "\n=======================================\n";
+    cout << " elemen array setelah di  urutkan (Asc)\n";
+    cout << "\n=======================================\n";
+
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j<  nPanjang - 1)
+        {
+            cout << " -> ";
+        }
+    }
+    cout << endl;
+}   
+
